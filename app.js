3068406b7889da2379345bdfaf1e752f9e868b9b
ВАШ НІКНЕЙМ <glib32586@gmail.com>
2026-05-06 22:39:00 +0300

    qwe

--- app.js
+++ app.js
@@ -13,3 +13,88 @@
         document.body.classList.toggle("no-scroll");
     }
 })();
+
+
+(() => {
+    const refs = {
+        openModalBtn: document.querySelector("[data-modal-open2]"),
+        closeModalBtn: document.querySelector("[data-modal-close2]"),
+        modal: document.querySelector("[data-modal2]"),
+    };
+
+    refs.openModalBtn.addEventListener("click", toggleModal);
+    refs.closeModalBtn.addEventListener("click", toggleModal);
+
+    function toggleModal() {
+        refs.modal.classList.toggle("is-hidden");
+        document.body.classList.toggle("no-scroll");
+    }
+})();
+
+
+(() => {
+    const refs = {
+        openModalBtn: document.querySelector("[data-modal-open3]"),
+        closeModalBtn: document.querySelector("[data-modal-close3]"),
+        modal: document.querySelector("[data-modal3]"),
+    };
+
+    refs.openModalBtn.addEventListener("click", toggleModal);
+    refs.closeModalBtn.addEventListener("click", toggleModal);
+
+    function toggleModal() {
+        refs.modal.classList.toggle("is-hidden");
+        document.body.classList.toggle("no-scroll");
+    }
+})();
+
+
+(() => {
+    const refs = {
+        openModalBtn: document.querySelector("[data-modal-open4]"),
+        closeModalBtn: document.querySelector("[data-modal-close4]"),
+        modal: document.querySelector("[data-modal4]"),
+    };
+
+    refs.openModalBtn.addEventListener("click", toggleModal);
+    refs.closeModalBtn.addEventListener("click", toggleModal);
+
+    function toggleModal() {
+        refs.modal.classList.toggle("is-hidden");
+        document.body.classList.toggle("no-scroll");
+    }
+})();
+
+
+(() => {
+    const refs = {
+        openModalBtn: document.querySelector("[data-modal-open5]"),
+        closeModalBtn: document.querySelector("[data-modal-close5]"),
+        modal: document.querySelector("[data-modal5]"),
+    };
+
+    refs.openModalBtn.addEventListener("click", toggleModal);
+    refs.closeModalBtn.addEventListener("click", toggleModal);
+
+    function toggleModal() {
+        refs.modal.classList.toggle("is-hidden");
+        document.body.classList.toggle("no-scroll");
+    }
+})();
+
+
+(() => {
+    const refs = {
+        openModalBtn: document.querySelector("[data-modal-open6]"),
+        closeModalBtn: document.querySelector("[data-modal-close6]"),
+        modal: document.querySelector("[data-modal6]"),
+    };
+
+    refs.openModalBtn.addEventListener("click", toggleModal);
+    refs.closeModalBtn.addEventListener("click", toggleModal);
+
+    function toggleModal() {
+        refs.modal.classList.toggle("is-hidden");
+        document.body.classList.toggle("no-scroll");
+    }
+})();
